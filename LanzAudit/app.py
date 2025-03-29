e097fe07403d1b091e583ffd209dd7c4358a37a9
Xerezanoo <juangarcialanza120805@gmail.com>
2025-03-29 15:03:20 +0100

    Refactor

--- LanzAudit/app.py
+++ LanzAudit/app.py
@@ -14,5 +14,9 @@ def forgotPassword():
 def home():
     return render_template("index.html")
 
+@app.route('/license')
+def license():
+    return render_template("license.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
